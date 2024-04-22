7a02a3f39dd82a3ef667f0790782d7dc02248d7f
shahzad006 <160237343+shahzad006@users.noreply.github.com>
2024-04-21 20:35:35 -0700

    first commit

---
+++ index.ts
@@ -0,0 +1,63 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+
+
+let currency: any ={
+    USD: 1, // Base currency
+    EUR: 0.91,
+    GBP: 0.76,
+    INR: 74.57,
+    PKR: 280 
+};
+
+let user_answer=await inquirer.prompt(
+
+    [
+        
+        {
+         name:"from",
+         message:"Enter From Currency",
+         type:"list",
+         choices:["USD", "EUR", "GBP", "INR", "PKR"]
+        },
+
+        {
+         name:"to",
+         message:"Enter to Currency",
+         type:"list",
+         choices:["USD", "EUR", "GBP", "INR", "PKR"]
+
+        },
+
+        {
+     
+         name:"amount",
+         message : "Enter yuor Amount",
+         type:"number"
+        }
+
+
+    
+    ]
+
+);
+
+
+
+
+let fromAmount = currency[user_answer.from]
+let toAmount = currency[user_answer.to]
+let amount = user_answer.amount;
+let baseAmount = amount/fromAmount ;
+let convertedAmount=baseAmount*toAmount ; 
+console.log(convertedAmount.toFixed(4))
+
+
+//console.log(`${amount} ${user_answer.from} is equal to ${convertedAmount.toFixed(4)} ${user_answer.to}`);
+
+
+// console.log(fromAmount);
+// console.log(toAmount);
+// console.log(amount);
